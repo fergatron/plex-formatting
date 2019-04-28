57db5c2d2ea54ce9bf358c2584723865e91fc585
Marc Ferguson <marc@fergytech.com>
2019-04-28 00:06:10 -0400

    :construction: Working on global variables within loops.

--- format4plex.sh
+++ format4plex.sh
@@ -7,15 +7,38 @@
 # The MVP of this script will just rename file names to be lowercase and replace
 # spaces with hypens. I'll worry myself with more advanced stuff later.
 
-# root parameter(s) to the script
-# $1 - directory
+# TODO
+# 1. Figure out how to convert '1x01' to 's01e01'
+# 2. Remove certain patterns (use external file as a database) - have user input pattern to remove
+# if it only removes formatting then don't go through the entire process of filtering and renaming
+# ie: --pattern-to-remove DVDrip.Xvid.mp3
 
-lines_read=0
-files_processed=0
+# root parameter(s) to the script
+# $1 - directory or OPTIONS
+# $2 - OPTIONS
+
+# global variables
+declare -i lines_read=0
+declare -i files_processed=0
 
 # remove temporarily files and directory
 clean_mock_files() {
     rm -rf tmp
+    printf "Successfully removed the tmp directory.\n"
+}
+
+# display final results and some stats
+display_results() {
+    echo
+    printf "Lines read: %s\n" ${lines_read}
+
+    if [ files_processed -gt 0 ]
+    then
+        printf "Directory list:\n"
+        ls
+    else
+        printf "No files successfully processed.\n"
+    fi
 }
 
 # uses the data from a previous command and processes the output
@@ -23,7 +46,7 @@ process_list() {
     IFS=""
     while read -r line
     do
-        (( lines_read += 1 ))
+        lines_read=$(( $lines_read + 1 ))
         lowercase="$(echo ${line} | tr '[:upper:]' '[:lower:]')" # convert to lowercase
         lowercase="$(echo ${lowercase} | xargs -0)" # remove trailing whitespace
         lowercase=$(echo $lowercase | sed 's/ /-/g') # replace spaces with hypens
@@ -33,15 +56,17 @@ process_list() {
         lowercase=$(echo ${lowercase} | tr '\342\200\262' '_')
         lowercase=$(echo ${lowercase} | tr '\47' '_') # '
         lowercase=$(echo ${lowercase} | tr ',' '_')
+        lowercase=$(echo ${lowercase} | tr '(' '_')
+        lowercase=$(echo ${lowercase} | tr ')' '_')
 
-        # printf '%s\n' ${lowercase}
-        mv ${line} ${lowercase}
+        # actually processing the file based on filters
+        # $(mv ${line} ${lowercase})
+        # $(echo $lowercase)
+        if [ $(mv ${line} ${lowercase}) ]; then
+            files_processed=$(( $files_processed + 1 ))
+        fi
     done
-
-    echo
-    printf "Lines read: %s\n" ${lines_read}
-    printf "Directory list:\n"
-    ls
+    printf "lines read (%s) processed (%s)." ${lines_read} ${files_processed}
 }
 
 # seed a temporary directory with mock files in order to test this script
@@ -50,15 +75,18 @@ seed_mock_files() {
     if [ ! -d "./tmp" ]
     then
         mkdir -p tmp
+        printf "Successfully created tmp directory.\n"
     fi
 
     mockFiles=(
         'ALF-S02E01-Working My Way Back to You.mp4'
         'ALF-S02E02-Somewhere Over the Rerun (aka The Ballad of Gilligan'\''s Island).avi'
         'ALF-S02E03-Take, a Look at Me Now.avi'
-        "ALF-S02E08-Something's Wrong With Me.avi"
         "ALF-S02E12-ALF's Special Christmas.avi"
-        'ALF-S02E18-We Gotta Get Out of This Place.avi'
+        'Full House 1x00 - Pilot Unaired Pilot(Dvdrip)(Dark_Stalker).avi'
+        'Full House 1x02 - Our Very First Night(Dvdrip)(Dark_Stalker).avi'
+        'Darkwing Duck - 122 - Double Darkwings.avi'
+        'Darkwing Duck - 123 - Aduckyphobia.avi'
     )
     IFS=""
     for file in ${mockFiles[@]}
@@ -66,6 +94,7 @@ seed_mock_files() {
         # printf "%s\n" ${file}
         touch tmp/${file}
     done
+    printf "Successfully created temporary files.\n"
 }
 
 # PROGRAM EXECUTES HERE
@@ -84,6 +113,7 @@ then
                 ;;
             *)
                 ls $var | process_list
+                display_results
         esac
     done
 else
